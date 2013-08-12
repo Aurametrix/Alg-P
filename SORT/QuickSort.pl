9d84a7a7d8425ccee829c4e9ffac2f334da6a003
Irene <irene@aurametrix.com>
2013-08-12 08:07:01 -0700

    QuickSort

---
+++ SORT/QuickSort.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+
+sub qsort (\@) {_qsort($_[0], 0, $#{$_[0]})}
+
+sub _qsort {
+    my ($array, $low, $high) = @_;
+    if ($low < $high) {
+        my $mid = partition($array, $low, $high);
+        _qsort($array, $low,     $mid - 1);
+        _qsort($array, $mid + 1, $high   );
+    }
+}
+
+sub partition {
+    my ($array, $low, $high) = @_;
+    my $x = $$array[$high];
+    my $i = $low - 1;
+    for my $j ($low .. $high - 1) {
+        if ($$array[$j] <= $x) {
+            $i++;
+            @$array[$i, $j] = @$array[$j, $i];
+        }
+    }
+    $i++;
+    @$array[$i, $high] = @$array[$high, $i];
+    return $i;
+}
+
