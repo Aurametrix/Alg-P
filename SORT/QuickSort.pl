685909f2b7c4431231250cb78cea456940ea7719
Irene <irene@aurametrix.com>
2013-08-13 08:18:31 -0700

    added input output

--- SORT/QuickSort.pl
+++ SORT/QuickSort.pl
@@ -27,3 +27,32 @@ sub partition {
     return $i;
 }
 
+if ($#ARGV < 0) {
+    print q{
+=============================================================
+|                                                           |
+|               Please type a few numbers                   |
+|             and the script will sort them                 |
+|                                                           |
+|-----------------------------------------------------------|
+|             Syntax - file.pl ARG1 ARG2 ARG3               |
+|                                                           |
+=============================================================
+};
+    exit;
+                   }
+
+elsif ($#ARGV < 2) { 
+print length(@ARGV);
+}
+
+foreach my $arg (@ARGV) {
+    print $arg, "\n";
+}
+
+#my @array = (8, 5, 9, 2, 1, 7, 3, 4);
+#qsort @array;
+#print "@array\n"; # 1 2 3 4 5 7 8 9
+
+qsort @ARGV;
+print "@ARGV\n";
