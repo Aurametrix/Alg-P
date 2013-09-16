79e1b04e568f597cfac81be29de09cc1834f6b4b
Irene <irene@aurametrix.com>
2013-09-16 14:39:45 -0700

    hashes and awways

--- HASH/hash_sort.pl
+++ HASH/hash_sort.pl
@@ -35,27 +35,38 @@ sub make_hash (@) {
 #%hash=( 89=>3, 45=>2, 1 =>5, 40=>3);
 
 
-my $key2 = 2;
+my $key2 = 9;
+my $value = "x";
+my @array = (2,3,5);
 
 my %hash 
     = make_hash(
       '89' => 3
     , '45' => $key2
-    , '3' => 90
+    , '3' => 'ref:45'
     , '1' => 'ref:key1'
-    , '40' => 'key2'
-    , '50' => 'lit:lit:ref:key3'
+    , '40' => 'lit:ref:89'
+    , '50' => 'lit:lit:ref:3'
     );
 
-# PRINT THE HASH
-print %hash;
+push ( @array, 6 );
+print "plain array: ", @array;
+$hash{$key2} = $value;
 
-print "\n sorted hash:\n"; 
+# PRINT THE HASH
+print "plain hash: ", %hash;
+
+print "\nhash sorted by values:\n"; 
 foreach (sort { $b <=> $a } keys(%hash) )
 {
 	print "key: $_ value: $hash{$_}\n"
 }
 
+print "\nhash sorted by keys:\n"; 
 foreach my $firstcolumn (sort keys %hash) {
         print $firstcolumn . ": value: " . $hash{$firstcolumn} . "\n";
     }
+    
+    
+#my $x=join(':','a','b'); 
+#print $x, "\n";
