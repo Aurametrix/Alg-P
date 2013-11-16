dce77ce10da3af8c45fd0768a8bc24af340300a4
Irene <irene@aurametrix.com>
2013-11-16 12:20:59 -0800

    added some interactivity

--- REGEX/simple.pl
+++ REGEX/simple.pl
@@ -14,16 +14,22 @@ my $new_text = modify('foo baz', sub { $_[0] =~ s/foo/bar/ });
 print "here's $new_text\n";
 
 
-my $mystring = "Hello world!";
+my $mystring=join(' ',@ARGV);
 my $regex = "Hello";
 
-if ($mystring =~ m/($regex)/) {
-  print "match\n";
-} else {
-  print "no match\n";
+if (@ARGV == 0 && -t STDIN && -t STDERR) { 
+    print STDERR "$0: you didn't input anything from keyboard\n";
+    $mystring = "Hello world!";
+    print "I will use example sentence: $mystring\n";
 }
 
+if ($mystring =~ m/($regex)/) {
+  print "exact match to $regex\n";
+} else {
+  print "no exact match to $regex\n";
+}
 
+# ignore case
 $mystring =~ s/hello/Goodbye/i;
 print $mystring;
 print "\n";
