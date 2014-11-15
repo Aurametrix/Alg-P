042b2b2ebeafba1b18d4edc9cebcc5e1b1a6addd
Irene <irene@aurametrix.com>
2014-11-15 15:19:08 -0800

    to add new Car subclasses without modifying ParkingLot class

--- OO/parking_lot.pl
+++ OO/parking_lot.pl
@@ -1,19 +1,23 @@
 package Car;
 
+sub park {
+  # park a generic Car
+}
+
 package Ferrari;
 use base 'Car';
+sub park {
+  # take the Ferrari for a spin before parking it
+  $self->drive_fast;
+  $self->SUPER::park;
+}
 
 package Chevrolet;
 use base 'Car';
+# No sub park defined, so it just parks like a generic Car
 
 package ParkingLot;
 
 sub add_car {
-  if (ref $car = 'Ferrari') {
-    $self->park_ferrari;
-  } elsif (ref $car = 'Chevrolet') {
-    $self->park_chevrolet;
-  } else {
-    die "Unknown car model!";
-  }
+$car->park;
 }
