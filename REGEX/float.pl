95fdef4a5620c243d924fe20c43d45997b912898
Aurametrix <irene@aurametrix.com>
2015-04-03 16:33:49 -0700

    check if a floating point number

--- REGEX/float.pl
+++ REGEX/float.pl
@@ -4,3 +4,9 @@ Math::BigFloat->accuracy(12);
 $n = new Math::BigFloat "52.12";
 
 print "Accuracy of $n is ", $n->accuracy(), " length ",scalar($n->length()),"\n";
+
+# is it a digit?
+if($n =~ m/\d/) { print "Yes, it is a digit \n"; }
+#print " More about $n \n";
+# is it a float?
+if($n =~ m/^\d+.\d+$/) { print "Yes, it is a float \n"; }
