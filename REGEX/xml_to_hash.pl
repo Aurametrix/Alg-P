1e1097a8505e31d730a5170542db6e9680a98ef5
Aurametrix <irene@aurametrix.com>
2015-04-22 15:26:35 -0700

    print all titles

--- REGEX/xml_to_hash.pl
+++ REGEX/xml_to_hash.pl
@@ -7,12 +7,18 @@ use strict;
 use XML::Simple;
 # use Data::Dumper;
 
-#my $booklist = XMLin('examples/booklist.xml');
+my $booklist = XMLin('examples/booklist.xml');
+
+foreach my $book (@{$booklist->{book}}) {
+	print $book->{title} . "\n";
+}
 
 # using  KeyAttr => {book => 'isbn'} to tell the parser to create
 # a data structure that uses the isbn element as a lookup key
-my $booklist = XMLin('examples/booklist.xml', KeyAttr => {book => 'isbn'});
+## my $booklist = XMLin('examples/booklist.xml', KeyAttr => {book => 'isbn'});
 
 #print Dumper($booklist);
 
-print $booklist->{book}->{Book2ISBN}->{title} . "\n";
+# Print the title of the book with isbn Book2ISBN
+## print $booklist->{book}->{Book2ISBN}->{title} . "\n";
+
